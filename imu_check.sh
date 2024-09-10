df1fa09a3a727d77a46c7587871e852805e948cc
Beagle User <debian@BeagleBone.localdomain>
2024-09-10 00:22:04 +0000

    Code compile

--- imu_check.sh
+++ imu_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Adresse du capteur IMU (0x68) et registre d'identification (0x71)
-ID=$(i2cget -f -y 2 0x68 0x71)
-EXPECTED_ID="0x75"
+ID=$(i2cget -f -y 2 0x68 0x75)
+EXPECTED_ID="0x71"
 
 if [ "$ID" == "$EXPECTED_ID" ]; then
     echo "Le capteur IMU est present."
