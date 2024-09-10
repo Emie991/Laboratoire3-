f506eb96e9096ca86c924237c0d9976a47a3fcf2
Beagle User <debian@BeagleBone.localdomain>
2024-09-10 00:19:37 +0000

    Ajout programme en c

--- test_sensors.c
+++ test_sensors.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() 
+int main()
 {
-    // Appel du script pour vérifier le baromètre
-    printf("Vérification du baromètre...\n");
     system("./barometer_check.sh");
-
-    // Appel du script pour vérifier l'accéléromètre/gyroscope/magnétomètre
-    printf("Vérification de l'IMU...\n");
     system("./imu_check.sh");
-
     return 0;
 }
